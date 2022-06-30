a9965cad1c17fd823e4f7614641397a15ba05659
Md-Swapan <swapan@excelitai.com>
2022-06-30 17:56:53 +0600

    nav update

---
+++ components/SharedComponent/Nav.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="navbar">
+    <div class="container">
+
+        <ul>
+            <li><nuxt-link to="/">Home</nuxt-link></li>
+            <li><nuxt-link to="/about">Product</nuxt-link></li>
+            <li><nuxt-link to="/product">Product</nuxt-link></li>
+        </ul>
+    </div>
+  </div>
+</template>
