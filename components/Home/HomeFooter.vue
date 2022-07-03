16a55209e263c820732a4c714424fdcf710d0e81
Md-Swapan <swapan@excelitai.com>
2022-07-03 11:44:32 +0600

    Home footer done

--- components/Home/HomeFooter.vue
+++ components/Home/HomeFooter.vue
@@ -29,14 +29,63 @@
             </div>
 
             <div class="col-md-3">
-                WE ARE HERE TO HELP ! FAQ 24/7 CUSTOMER SUPPORT 09678822444
+                <div class="ft-section2">
+                    <h2>WE ARE HERE TO HELP!</h2>
+                    <h3>FAQ</h3>
+                    <h3>24/7 CUSTOMER SUPPORT</h3>
+                    <h3><i class="fa-solid fa-phone"></i> 09678822444</h3>
+                </div>
             </div>
             <div class="col-md-3">
-                KNOW US BETTER About Us Contact Us Privacy Policy Terms and Conditions
-                Return Policy
+                <div class="ft-section3">
+                    <h2>KNOW US BETTER</h2>
+                    <p>About Us</p>
+                    <p>Contact Us</p>
+                    <p>Privacy Policy</p>
+                    <p>Terms and Conditions</p>
+                    <p>Return Policy</p>
+                </div>
+
             </div>
             <div class="col-md-3">
-                MAKE MONEY WITH US Sell on BPPSHOPS Seller term & conditions
+                <div class="ft-section4">
+                    <h2>MAKE MONEY WITH US</h2>
+                    <p>Sell on BPPSHOPS</p>
+                    <p>Seller term & conditions</p>
+                    <button class="subscribeBtn">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        Subscribe
+                    </button>
+                    <div><a href=""><img src="/img/apple-app-store.png" alt=""></a></div>
+                    <div><a href=""><img src="/img/google-play-store.png" alt=""></a></div>
+                </div>
+
+            </div>
+        </div>
+        <div class="row">
+            <div class="paywith-container-icons">
+                <h4>Pay With</h4>
+                <div class="payIcons">
+                    <img width="70" src="/img/icons/bppshops_ssl.webp" alt="">
+                    <img width="70" src="/img/icons/bppshops_ab_bank.webp" alt="">
+                    <img width="70" src="/img/icons/bppshops_brack_bank.webp" alt="">
+                    <img width="70" src="/img/icons/bppshops_city_bank.webp" alt="">
+                    <img width="40" src="/img/icons/bppshops_islamic.webp" alt="">
+                    <img width="70" src="/img/icons/bppshops_bk_asia.webp" alt="">
+                    <img width="70" src="/img/icons/bppshops_dbbl.webp" alt="">
+                    <img width="40" src="/img/icons/bppshops_q.webp" alt="">
+                    <img width="70" src="/img/icons/bppshops_bk.webp" alt="">
+                    <img width="70" src="/img/icons/bppshops_mtb.webp" alt="">
+                    <img width="70" src="/img/icons/bppshops_sure_cash.webp" alt="">
+                    <img width="40" src="/img/icons/bppshops_oupy.webp" alt="">
+                    <img width="40" src="/img/icons/bppshops_m_cash.webp" alt="">
+                    <img width="40" src="/img/icons/bppshops_-_cash.webp" alt="">
+                    <img width="40" src="/img/icons/bppshops_my.webp" alt="">
+                    <img width="40" src="/img/icons/bppshops_fast.webp" alt="">
+                </div>
             </div>
         </div>
     </div>
@@ -51,9 +100,10 @@ export default {
 
 <style scoped>
 .footer-container {
-    height: 350px;
+    height: 530px;
     padding: 40px 0px;
     margin-top: 20px;
+    margin-bottom: -20px;
     background-color: #000000;
     color: white;
     font-family: "Rajdhani", sans-serif;
@@ -61,10 +111,12 @@ export default {
 
 .ft-section1 img {
     width: 13em;
+    margin-left: -15px;
 }
 
 .social-icons {
     margin: 20px;
+    margin-left: 0px;
 }
 
 .social-icons li {
@@ -100,9 +152,10 @@ export default {
     font-size: 15px;
 }
 
-.location-email-number{
+.location-email-number {
     margin-top: 50px;
 }
+
 .location-email-number h6 {
     font-size: 15px;
     margin: 10px 0px;
@@ -110,8 +163,148 @@ export default {
 }
 
 .location-email-number h6 i {
-    color: goldenrod;
+    color: #FAA919;
     padding-right: 10px;
     font-size: 20px;
+}
+
+.ft-section2 h2 {
+    font-weight: 500;
+    margin-bottom: 20px;
+    color: #FAA919;
+}
+
+.ft-section2 h3 {
+    font-weight: 500;
+    color: rgb(194, 194, 194);
+    margin-bottom: 20px;
+}
+
+.ft-section2 h3 i {
+    color: #FAA919;
+    margin-right: 10px;
+}
+
+.ft-section3 h2,
+.ft-section4 h2 {
+    color: #FAA919;
+    font-weight: 500;
+}
+
+.ft-section3 p,
+.ft-section4 p {
+    color: rgb(194, 194, 194);
+    font-weight: 400;
+    margin: 10px 0px;
+    cursor: pointer;
+}
+
+.subscribeBtn{
+    outline: none;
+    padding: 12px 40px;
+    text-transform: uppercase;
+    position: relative;
+    overflow: hidden;
+    margin-top:10px;
+    margin-bottom: 20px;
+}
+
+.subscribeBtn:hover{
+	color: #FAA919;
+}
+.subscribeBtn span:nth-child(1){
+	position: absolute;
+    top: 0;
+		left: 0;
+		width: 100%;
+		height: 1px;
+		background: linear-gradient(to right, #ffffff8c, #FAA919);
+		animation: animate1 2s linear infinite;
+}
+@keyframes animate1{
+	0%{
+		transform: translateX(-100%);
+	}
+	100%{
+		transform: translateX(100%);
+	}
+}
+.subscribeBtn span:nth-child(2){
+	position: absolute;
+    top: 0;
+		right: 0;
+		width: 1px;
+		height: 100%;
+		background: linear-gradient(to bottom, #ffffff8c, #FAA919);
+		animation: animate2 2s linear infinite;
+		animation-delay: 1s;
+}
+@keyframes animate2{
+	0%{
+		transform: translateY(-100%);
+	}
+	100%{
+		transform: translateY(100%);
+	}
+}
+.subscribeBtn span:nth-child(3){
+	position: absolute;
+    bottom: 0;
+		left: 0;
+		width: 100%;
+		height: 1px;
+		background: linear-gradient(to left, #ffffff8c, #FAA919);
+		animation: animate3 2s linear infinite;
+}
+@keyframes animate3{
+	0%{
+		transform: translateX(100%);
+	}
+	100%{
+		transform: translateX(-100%);
+	}
+}
+.subscribeBtn span:nth-child(4){
+	position: absolute;
+    top: 0;
+		left: 0;
+		width: 1px;
+		height: 100%;
+		background: linear-gradient(to top, #ffffff8c, #FAA919);
+		animation: animate4 2s linear infinite;
+		animation-delay: 1s;
+}
+@keyframes animate4{
+	0%{
+		transform: translateY(100%);
+	}
+	100%{
+		transform: translateY(-100%);
+	}
+}
+
+
+/* paywith-container-icons css*/
+.paywith-container-icons{
+    display: flex;
+    background: #fff;
+    width: 100%;
+    color: #5e5e5e;
+    align-items: center;
+    margin-top: 30px;
+    padding: 10px 0px;
+    border-radius: 5px;
+}
+.paywith-container-icons h4{
+    margin: 0px 50px;
+}
+.payIcons{
+    display: grid;
+    grid-template-columns: repeat(16, 1fr);
+    grid-gap: 30px;
+    align-items: center;
+}
+.payIcons img{
+    /* margin-top: 20px; */
 }
 </style>
