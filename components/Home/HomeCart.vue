fb36aeac30e948d4f7a4b8f3311e6f967465b6ba
ZAHID188 <zahidpranta5@gmail.com>
2022-07-05 10:28:51 +0600

    HomeCart routing functionality

--- components/Home/HomeCart.vue
+++ components/Home/HomeCart.vue
@@ -2,9 +2,11 @@
   <div class="container">
     <div class="cart-container">
       <li class="cart" v-for="item in category" :key="item.id">
-        <p v-if="item.status == 'Upcoming'">{{ item.status }}</p>
-        <img :src="item.img" alt="" />
-        <h3>{{ item.name }}</h3>
+        <nuxt-link :to="'/'+item.route">
+          <p v-if="item.status == 'Upcoming'">{{ item.status }}</p>
+          <img :src="item.img" alt="" />
+          <h3>{{ item.name }}</h3>
+        </nuxt-link>
       </li>
     </div>
   </div>
@@ -22,48 +24,63 @@ export default {
           name: "Islamic Product",
           img: "img/categoryImg/islamic_product.webp",
           status: "",
+          route:"islamic"
         },
         {
           id: 2,
           name: "Grocery",
           img: "img/categoryImg/grocery.webp",
           status: "",
+          route:"grocery"
+
         },
         {
           id: 3,
           name: "Fashion",
           img: "img/categoryImg/fashion.webp",
           status: "",
+          route:"Fashion"
+
         },
         {
           id: 4,
           name: "Electronics",
           img: "img/categoryImg/electronics.webp",
           status: "Upcoming",
+          route:"Electronics"
+
         },
         {
           id: 5,
           name: "Baby Care",
           img: "img/categoryImg/baby_care.webp",
           status: "Upcoming",
+          route:"BabyCare"
+
         },
         {
           id: 6,
           name: "Cosmetics",
           img: "img/categoryImg/cosmetic.webp",
           status: "Upcoming",
+          route:"Cosmetics"
+
         },
         {
           id: 7,
           name: "Furniture",
           img: "img/categoryImg/furniture.webp",
           status: "Upcoming",
+          route:"Furniture"
+
         },
         {
           id: 8,
           name: "Shoes",
           img: "img/categoryImg/shoe.webp",
           status: "Upcoming",
+          route:"Shoes"
+
         },
       ],
     };
@@ -72,6 +89,7 @@ export default {
 </script>
 
 <style scoped>
+
 .container {
   margin-top: 100px;
 }
@@ -136,7 +154,7 @@ export default {
 }
 @media screen and (max-width: 992px) {
   .cart-container {
-     width: 80%;
+    width: 80%;
     display: grid;
     grid-template-columns: repeat(3, 1fr);
     margin: auto;
---
+++ pages/islamic.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="islamic-container">
+        <h1>
+            This is islamic page
+        </h1>
+    </div>
+</template>
+
+<style scoped>
+.islamic-container{
+    margin: 100px;
+}
+</style>
