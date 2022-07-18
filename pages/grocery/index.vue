e350b58641f2250e57076dd1c848300d9f459cb3
Md-Swapan <swapan@excelitai.com>
2022-07-18 17:57:13 +0600

    vuex update

--- pages/grocery/index.vue
+++ pages/grocery/index.vue
@@ -7,20 +7,23 @@
         <div class="sidebar">
             <div class="sidebar-header">
                 <h2><i class="bi bi-list"></i> Shop by Category</h2>
-
-                <ul class="menu-list">
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                </ul>
             </div>
+            <ul class="menu-list">
+                <li>Lorem ipsum dolor sit.</li>
+                <li>Lorem ipsum dolor sit.</li>
+                <li>Lorem ipsum dolor sit.</li>
+                <li>Lorem ipsum dolor sit.</li>
+                <li>Lorem ipsum dolor sit.</li>
+                <li>Lorem ipsum dolor sit.</li>
+                <li>Lorem ipsum dolor sit.</li>
+                <li>Lorem ipsum dolor sit.</li>
+                <li>Lorem ipsum dolor sit.</li>
+                <li>Lorem ipsum dolor sit.</li>
+            </ul>
         </div>
         <div class="product-content">
 
-            <div  v-for="product in products" :key="product.id">
+            <div v-for="product in products" :key="product.id">
 
                 <div class="product-card">
                     <div class="img-container">
@@ -64,9 +67,7 @@ export default {
     //     this.products = result.data
     // },
 
-
-  
-    watch:{
+    watch: {
         products() {
             // console.log(this.$store.state.products);
             return this.$store.state.products;
@@ -83,7 +84,6 @@ export default {
         }
     }
 };
-
 </script>
 
 <style scoped>
@@ -98,9 +98,24 @@ export default {
 
 .sidebar {
     width: 18%;
+    height: 80vh;
     border: .01px solid rgb(233, 233, 233);
     padding: 10px 20px;
     border-radius: 5px;
+    /* position: sticky; */
+}
+.sidebar-header{
+    border-bottom: .5px solid #e6e6e6;
+}
+.sidebar-header h2{
+    margin: 10px 0px;
+}
+.menu-list li {
+    list-style: none;
+    margin: 20px 0px;
+    margin-left: 30px;
+    padding-bottom: 10px;
+    /* border-bottom: .1px solid #f1f1f1f1; */
 }
 
 .product-content {
